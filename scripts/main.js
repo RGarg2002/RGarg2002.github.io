377de96bc70b82d596af1322f105c54a3b731687
Royal Garg <royalgarg36@gmail.com>
2023-10-17 20:05:34 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/img1.jpg") {
+    myImage.setAttribute("src", "images/img2.png");
+  } else {
+    myImage.setAttribute("src", "images/img1.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+
+    if (!myName)
+    {
+        setUserName();
+    }
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+};
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+  
+myButton.onclick = () => {
+    setUserName();
+};
